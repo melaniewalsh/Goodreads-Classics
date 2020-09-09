530a52f05c3c9aa7d104d554cbb0b7808f35649f
melaniewalsh <melanie.walsh@cornell.edu>
2020-09-09 12:33:27 -0400

    update Topics viz for markdown file

--- js/heatmap.js
+++ js/heatmap.js
@@ -82,8 +82,8 @@ d3.csv("https://raw.githubusercontent.com/melaniewalsh/Goodreads-Classics/master
     tooltip
     //.attr("class", "word-wrap")
       .html(`<b>Title:</b> <i>${d.Classics}</i> <br> <b>Topic:</b> ${d.Topics} <br>  <b>Topic Probability:</b> ${d3.format(".2f")(d.value)} <br><br><b>Topic Words:</b> ${d['Topic Words']}`)
-      .style("left", (d3.mouse(this)[0]+200) + "px")
-      .style("top", (d3.mouse(this)[1]+500) + "px")
+      .style("left", (d3.mouse(this)[0]+100) + "px")
+      .style("top", (d3.mouse(this)[1]+200) + "px")
         
       //.style("max-width", "200px")
 	//.style("position", "absolute")
